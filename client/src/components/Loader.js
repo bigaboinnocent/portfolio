9acf97a5e868dacdc433eb728f1bf40222a56abc
bigaboinnocent <bigaboinnocent086@gmail.com>
2026-06-21 16:29:38 +0200

    Update Loader component names

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -4,8 +4,8 @@ function Loader() {
   return (
     <div className='h-screen flex items-center justify-center fixed inset-0 bg-primary z-[10000]'>
         <div className='flex gap-5 text-6xl font-semibold sm:text-3xl'>
-             <h1 className="text-secondary k">Kayitare</h1>
-             <h1 className="text-white s">Audax</h1>
+             <h1 className="text-secondary k">Bigabo</h1>
+             <h1 className="text-white s">Innocent</h1>
              <h1 className="text-tertiary r">Dev-portfolio</h1>
         </div>
     </div>
