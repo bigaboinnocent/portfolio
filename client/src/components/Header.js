68427b4d43bc933a2ad47fb9118f38d653a12013
bigaboinnocent <bigaboinnocent086@gmail.com>
2026-06-21 16:29:01 +0200

    Update header name from 'Kayitare Audax' to 'Bigabo Innocent'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
      <nav className="border-b-2 border-tertiary bg-primary px-6 py-3 flex justify-between ">
-      <h1 className="font-bold text-xl text-secondary">Kayitare Audax</h1>
+      <h1 className="font-bold text-xl text-secondary">Bigabo Innocent</h1>
       <div className="space-x-4 text-white">
         <Link to="/">Home</Link>
         <a href='#about'>About</a>
